2ecc56980286e776d6ad930ccb4bbbe369885a02
Girish Thanki <girish.thanki@gmail.com>
2019-02-03 20:36:56 +0000

    Change styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CarsListDemoComponent } from './cars-list-demo/cars-list-demo.component';
 import { DialogService } from 'primeng/primeng';
+import { style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'PrimeNgPanel';
@@ -17,7 +18,9 @@ export class AppComponent {
     const ref = this.dialogService.open(CarsListDemoComponent, {
         header: 'Choose a Car',
         width: '70%',
-        height: '500px'
+        height: '500px',    
+        contentStyle: {"overflow": "auto"},
+        styleClass: 'dialog-filters'        
     });
   }
 }
